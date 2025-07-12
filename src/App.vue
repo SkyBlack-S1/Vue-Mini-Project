6a3f70571b881303d6002b9b6b4ccc7a8b27a12e
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-12 23:55:43 +0700

    Khởi tạo dự án + Xây dựng Header
    Gồm 2 trang Home & Create Todo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <img
+      class="logo"
+      width="60"
+      height="60"
+      src="@/assets/logo.svg"
+      alt="Vue Logo"
+    />
+    <nav>
+      <div class="btn-main">
+        <RouterLink to="/">Home Todo</RouterLink>
+      </div>
+      <div class="btn-action">
+        <RouterLink to="/create">Create Todo</RouterLink>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.4rem 2rem;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.15);
+}
+
+nav {
+  display: flex;
+  column-gap: 1rem;
+}
+</style>
