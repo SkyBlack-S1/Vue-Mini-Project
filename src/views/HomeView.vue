4b70e3403aa5d9a6c79d275143f919e010d59a72
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-13 15:51:56 +0700

    Fetch Data API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,30 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const users = ref(0);
+onMounted(() => {
+  // Cách 1
+  (async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    users.value = data;
+  })();
+  //()() dấu đầu tiên là của hàm, dấu thứ hai là thực thi hàm
+
+  // Cách 2
+  // fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then((response) => response.json())
+  //   .then((json) => (users.value = json));
+});
+</script>
 
 <template>
   <main style="color: #000; padding: 2rem">
     <input type="text" placeholder="Enter Search Here!..." />
     <div class="group-card">
-      <div class="card-item">
-        <h2>SkyBlack</h2>
-        <i>Hello</i>
-      </div>
-      <div class="card-item">
-        <h2>SkyBlue</h2>
-        <i>Hello</i>
+      <div class="card-item" v-for="user in users">
+        <h2>{{ user.name }}</h2>
+        <b>{{ user.email }}</b>
       </div>
     </div>
   </main>
